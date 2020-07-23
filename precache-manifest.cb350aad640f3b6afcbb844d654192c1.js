d922bd16ed2ffcda11f01513c0c2619b25af5e66
cuongvm <cuongvm2@smartosc.com>
2020-07-23 17:08:10 +0700

    Updates

---
+++ precache-manifest.cb350aad640f3b6afcbb844d654192c1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d180e67d34c464f0938a8e29c4c450a1",
+    "url": "/blackwolf25/react-redux-shopping-cart/index.html"
+  },
+  {
+    "revision": "ca550b25e5a9f14047be",
+    "url": "/blackwolf25/react-redux-shopping-cart/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "7b840c357d81b83eff3b",
+    "url": "/blackwolf25/react-redux-shopping-cart/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "ca550b25e5a9f14047be",
+    "url": "/blackwolf25/react-redux-shopping-cart/static/js/2.7d0fc60f.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/blackwolf25/react-redux-shopping-cart/static/js/2.7d0fc60f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7b840c357d81b83eff3b",
+    "url": "/blackwolf25/react-redux-shopping-cart/static/js/main.4cd23333.chunk.js"
+  },
+  {
+    "revision": "6543101e5975c531386a",
+    "url": "/blackwolf25/react-redux-shopping-cart/static/js/runtime-main.41c929ac.js"
+  }
+]);
